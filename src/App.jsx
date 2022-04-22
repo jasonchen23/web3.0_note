b268cfe7b676ffb44016a043fdf5e10ce1e3ca23
Jason <jason@example.com>
2022-04-23 02:52:02 +0800

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,17 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React from 'react';
+import './App.css';
+import MetaMaskAuth from "./components/metamask-auth";
+import MetaMaskLogo from "./favicon.svg"
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [msg, setMsg] = useState('hello world')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{msg}</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+    <main>
+      <div className="logoContainer">
+        <img src={MetaMaskLogo} height={90}/>
+      </div>
+      <MetaMaskAuth onAddressChanged={address => {}}/>
+    </main>
+  );
 }
 
-export default App
+export default App;
